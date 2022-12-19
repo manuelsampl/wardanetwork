ad110d5062cbf3cdf2336d71033801873ffcdd4b
Manuel Sampl <manuelsampl@MacBook-Pro-von-Manuel.local>
2022-12-19 08:37:16 +0100

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,24 +1,61 @@
-import * as React from "react"
-import { Container, Row, Col } from 'react-bootstrap'
-import { Link } from "gatsby"
+import React, { Suspense } from 'react'
+
+import * as THREE from "three"
+import { Canvas, useFrame } from "@react-three/fiber"
+import { Link } from 'gatsby'
+import Model from "./model"
+import useMousePosition from "../hooks/useMousePosition/useMousePosition"
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import Footer from '../components/footer/footer'
+import Header from '../components/header/header'
 
 
+function Rig() {
 
-const NotFoundPage = () => {
-  return (
-    <main >
-      <Container className="page-wrapper standard-page ">
-        <Row>
-          <Col xs={12}>
-            <h1>404 - WARDA NIX?</h1>
-          </Col>
-        </Row>
-      </Container>
+    let { x, y } = useMousePosition()
+    return useFrame((state) => {
 
-    </main>
-  )
+        console.log(THREE.MathUtils.lerp(state.camera.position.y, 1.5 + (y / 1000) / 2, 0.5))
+        state.scene.children[2].rotation.x = THREE.MathUtils.lerp(state.scene.children[2].rotation.x, -0.7 + (y / 1000) / 0.5, 0.5)
+        state.scene.children[2].rotation.y = THREE.MathUtils.lerp(state.scene.children[2].rotation.y, - 0.95 + (x / 1000) / 0.8, 0.5)
+
+
+    })
 }
 
-export default NotFoundPage
+export default function NotFoundPage({ context }) {
 
-export const Head = () => <title>Not found</title>
+
+
+    return (
+        <>
+            <Header></Header>
+            <Container className="page-wrapper standard-page page-404">
+                <Row>
+                    <Col xs={12}>
+                        <h1>404 - Da WARDA nix.</h1>
+                        <p>Such weiter...</p>
+                        <Link className="btn btn-black" to="/">Zur website</Link>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={12} className="three" id="three">
+                        <Canvas camera={{ position: [0, -1, 16], fov: 50 }}>
+                            <ambientLight />
+                            <directionalLight position={[-5, 5, 5]} />
+                            <group position={[0, 0, 5]}>
+                                <Suspense fallback={null}>
+                                    <Model />
+                                </Suspense>
+                            </group>
+                            <Rig></Rig>
+                        </Canvas>
+                    </Col>
+                </Row>
+            </Container>
+            <Footer></Footer>
+        </>
+    )
+}
